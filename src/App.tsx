2f4282c81c03d22d71d8b8638d101a081faeaeb3
Paul <paulwharin@gmail.com>
2023-12-04 15:04:17 +0000

    refactor: display CatCard component in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import Cat from "./data/cat";
 import { useState } from "react";
+import CatCard from "./components/cat_card";
 
 const initialCats = [
   {
@@ -90,29 +91,7 @@ function App(): JSX.Element {
 
       <main>
         <div className="cards__wrapper">
-          <div className="card">
-            <h3 className="card__text card__header">Cindy Clawford</h3>
-            <img
-              className="card__image"
-              src="someImageUrlHere"
-              alt="A cute tabby kitten"
-            ></img>
-            <p className="card__text">Species: Cat</p>
-            <p className="card__text">Favourite Food(s): Mice</p>
-            <p className="card__text">Birth Year: 2012</p>
-          </div>
-
-          <div className="card">
-            <h3 className="card__text card__header">Little Miss Purrfect</h3>
-            <img
-              className="card__image"
-              src="someImageUrlHere"
-              alt="A gray fluffy kitten"
-            ></img>
-            <p className="card__text">Species: Cat</p>
-            <p className="card__text">Favourite Food(s): Wet food, dry food</p>
-            <p className="card__text">Birth Year: 2016</p>
-          </div>
+          <CatCard />
         </div>
       </main>
 
