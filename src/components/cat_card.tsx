b7bb6acf91c85e12fb34448af0062f0e88f17c95
Paul <paulwharin@gmail.com>
2023-12-04 18:34:55 +0000

    feat: output fav foods as a comma delimited string

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -130,7 +130,7 @@ function CatCard({
     <div className="card">
       <h3 className="card__text card__header">{name}</h3>
       <p className="card__text">Species: {species}</p>
-      <p className="card__text">Favourite Food(s): {favFoods}</p>
+      <p className="card__text">Favourite Food(s): {favFoods.join(", ")}</p>
       <p className="card__text">Birth Year: {birthYear}</p>
 
       {catIndex < images.length && (
